12142a67c4e1af20f3d0eed9e24a11b9a339b126
rusnandi.fikri <rusnandi.fikri@cpssoft.com>
2025-09-01 11:26:07 +0700

    ok

--- project_aja.py
+++ project_aja.py
@@ -16,98 +16,100 @@ QDRANT_URL = st.secrets.get("QDRANT_URL") or os.getenv("QDRANT_URL")
 QDRANT_API_KEY = st.secrets.get("QDRANT_API_KEY") or os.getenv("QDRANT_API_KEY")
 OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY") or os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    api_key = OPENAI_API_KEY
-)
+st.write(f"url : {QDRANT_URL}")
 
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-small",
-    api_key = OPENAI_API_KEY)
+# llm = ChatOpenAI(
+#     model="gpt-4o-mini",
+#     api_key = OPENAI_API_KEY
+# )
 
-collection_name = "small_recipes"
-qdrant = QdrantVectorStore.from_existing_collection(
-    embedding=embeddings,
-    collection_name=collection_name,
-    url=QDRANT_URL,
-    api_key=QDRANT_API_KEY
-)
+# embeddings = OpenAIEmbeddings(
+#     model="text-embedding-3-small",
+#     api_key = OPENAI_API_KEY)
 
-@tool
-def get_relevant_docs(question):
-  """Use this tools for get relevant documents about recipes."""
-  results = qdrant.similarity_search(
-      question,
-      k=5
-  )
-  return results
+# collection_name = "small_recipes"
+# qdrant = QdrantVectorStore.from_existing_collection(
+#     embedding=embeddings,
+#     collection_name=collection_name,
+#     url=QDRANT_URL,
+#     api_key=QDRANT_API_KEY
+# )
 
-tools = [get_relevant_docs]
+# @tool
+# def get_relevant_docs(question):
+#   """Use this tools for get relevant documents about recipes."""
+#   results = qdrant.similarity_search(
+#       question,
+#       k=5
+#   )
+#   return results
 
-def chat_chef(question, history):
-    agent = create_react_agent(
-        model=llm,
-        tools=tools,
-        prompt= f'''You are a master of any recipes. Answer only question about recipes and use given tools for get recipes details.'''
-    )
-    result = agent.invoke(
-        {"messages": [{"role": "user", "content": question}]}
-    )
-    answer = result["messages"][-1].content
+# tools = [get_relevant_docs]
 
-    total_input_tokens = 0
-    total_output_tokens = 0
+# def chat_chef(question, history):
+#     agent = create_react_agent(
+#         model=llm,
+#         tools=tools,
+#         prompt= f'''You are a master of any recipes. Answer only question about recipes and use given tools for get recipes details.'''
+#     )
+#     result = agent.invoke(
+#         {"messages": [{"role": "user", "content": question}]}
+#     )
+#     answer = result["messages"][-1].content
 
-    for message in result["messages"]:
-        if "usage_metadata" in message.response_metadata:
-            total_input_tokens += message.response_metadata["usage_metadata"]["input_tokens"]
-            total_output_tokens += message.response_metadata["usage_metadata"]["output_tokens"]
-        elif "token_usage" in message.response_metadata:
-            # Fallback for older or different structures
-            total_input_tokens += message.response_metadata["token_usage"].get("prompt_tokens", 0)
-            total_output_tokens += message.response_metadata["token_usage"].get("completion_tokens", 0)
+#     total_input_tokens = 0
+#     total_output_tokens = 0
 
-    price = 17_000*(total_input_tokens*0.15 + total_output_tokens*0.6)/1_000_000
-    response = {
-        "answer": answer,
-        "price": price,
-        "total_input_tokens": total_input_tokens,
-        "total_output_tokens": total_output_tokens
-    }
-    return response
+#     for message in result["messages"]:
+#         if "usage_metadata" in message.response_metadata:
+#             total_input_tokens += message.response_metadata["usage_metadata"]["input_tokens"]
+#             total_output_tokens += message.response_metadata["usage_metadata"]["output_tokens"]
+#         elif "token_usage" in message.response_metadata:
+#             # Fallback for older or different structures
+#             total_input_tokens += message.response_metadata["token_usage"].get("prompt_tokens", 0)
+#             total_output_tokens += message.response_metadata["token_usage"].get("completion_tokens", 0)
 
-st.title("Chatbot Recipes Master")
-st.image("header_img.png")
+#     price = 17_000*(total_input_tokens*0.15 + total_output_tokens*0.6)/1_000_000
+#     response = {
+#         "answer": answer,
+#         "price": price,
+#         "total_input_tokens": total_input_tokens,
+#         "total_output_tokens": total_output_tokens
+#     }
+#     return response
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# st.title("Chatbot Recipes Master")
+# st.image("header_img.png")
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# # Initialize chat history
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
 
-# Accept user input
-if prompt := st.chat_input("Ask me recipes question"):
-    messages_history = st.session_state.get("messages", [])[-20:]
-    history = "\n".join([f'{msg["role"]}: {msg["content"]}' for msg in messages_history]) or " "
+# # Display chat messages from history on app rerun
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
 
-    # Display user message in chat message container
-    with st.chat_message("Human"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "Human", "content": prompt})
+# # Accept user input
+# if prompt := st.chat_input("Ask me recipes question"):
+#     messages_history = st.session_state.get("messages", [])[-20:]
+#     history = "\n".join([f'{msg["role"]}: {msg["content"]}' for msg in messages_history]) or " "
+
+#     # Display user message in chat message container
+#     with st.chat_message("Human"):
+#         st.markdown(prompt)
+#     # Add user message to chat history
+#     st.session_state.messages.append({"role": "Human", "content": prompt})
     
-    # Display assistant response in chat message container
-    with st.chat_message("AI"):
-        response = chat_chef(prompt, history)
-        answer = response["answer"]
-        st.markdown(answer)
-        st.session_state.messages.append({"role": "AI", "content": answer})
+#     # Display assistant response in chat message container
+#     with st.chat_message("AI"):
+#         response = chat_chef(prompt, history)
+#         answer = response["answer"]
+#         st.markdown(answer)
+#         st.session_state.messages.append({"role": "AI", "content": answer})
 
-    with st.expander("**History Chat:**"):
-        st.code(history)
+#     with st.expander("**History Chat:**"):
+#         st.code(history)
 
-    with st.expander("**Usage Details:**"):
-        st.code(f'input token : {response["total_input_tokens"]}\noutput token : {response["total_output_tokens"]}')
+#     with st.expander("**Usage Details:**"):
+#         st.code(f'input token : {response["total_input_tokens"]}\noutput token : {response["total_output_tokens"]}')
